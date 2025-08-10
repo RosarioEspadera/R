c367010c7633f8f451abece216702b203f5b7480
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-10 15:37:20 +0800

    Update supabase.js

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,3 +1,2 @@
-import { createClient } from '@supabase/supabase-js';
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 export const supabase = createClient('https://dtrjethplowazsxzhnwn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cmpldGhwbG93YXpzeHpobnduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MDc0MjgsImV4cCI6MjA3MDM4MzQyOH0.pCaRVB8lmipBGwEqBjeGlLbTxE2_IcvYw2yQ15qmxfo');
