902c14c11e0d024aeeb94b2f62ddcfa059233d55
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-10 09:25:19 +0000

    jhgf

--- scripts/user.js
+++ scripts/user.js
@@ -1,10 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = window.supabase.createClient(
-  "https://dtrjethplowazsxzhnwn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cmpldGhwbG93YXpzeHpobnduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MDc0MjgsImV4cCI6MjA3MDM4MzQyOH0.pCaRVB8lmipBGwEqBjeGlLbTxE2_IcvYw2yQ15qmxfo"
-);
-
+import { supabase } from "./supabase.js";
 
 const setForm = document.getElementById("set-form");
 const setNameInput = document.getElementById("set-name");
